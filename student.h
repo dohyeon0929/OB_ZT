9daba7fee51f0cc0c6ab55511f616699e80579f0
dohyeon0929 <eythgj3589@gmail.com>
2022-10-01 16:42:14 +0900

    기본 구조
    우선 각자 해당하는 클래스에 코드 짜고
    
    동작 메소드 호출은 Main_menu 클래스의 자기가 맡은 역할에 해당하는 메소드에 채워넣기

---
+++ student.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+class Student {
+private: 
+
+public:
+    Student(); 
+    
+};
+
+class Student_list : Student {
+private:
+
+public:
+    Student_list();
+};
+
+class Insertion {
+private: 
+
+public:
+    Insertion(); 
+
+};
+
+class Search {
+private: 
+
+public:
+    Search(); 
+
+};
+
+class Sorting {
+private:
+
+public:
+    Sorting();
+};
+
+class Main_menu {
+private:
+
+public:
+    Main_menu();
+};
